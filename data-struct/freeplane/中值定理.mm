aa5fd9d06f5ed003455fa2321356b6911dd4747c
liuxz <1510822551@qq.com>
2022-09-29 10:55:54 +0800

    jz

---
+++ data-struct/freeplane/中值定理.mm
@@ -0,0 +1,116 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="中值定理" FOLDED="false" ID="ID_1685834159" CREATED="1661658601835" MODIFIED="1661660057498" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#3c3f41">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" associatedTemplateLocation="template:/Darcula-1.7.mm" fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_773655486" ICON_SIZE="12 pt" COLOR="#cccccc" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_773655486" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#660000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#660000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="有界与最值定理" POSITION="right" ID="ID_1418215185" CREATED="1661658637847" MODIFIED="1661658674855">
+<edge COLOR="#00dddd"/>
+</node>
+<node TEXT="介值定理" POSITION="right" ID="ID_1695571673" CREATED="1661658675357" MODIFIED="1661658680650">
+<edge COLOR="#dddd00"/>
+</node>
+<node TEXT="平均值定理" POSITION="right" ID="ID_1321251210" CREATED="1661658681245" MODIFIED="1661658686976">
+<edge COLOR="#dd0000"/>
+</node>
+<node TEXT="零点定理" POSITION="right" ID="ID_1137819289" CREATED="1661658689558" MODIFIED="1661659858795">
+<edge COLOR="#00dd00"/>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      设f(x)在[a,b]上连续，当f(a)*f(b)&lt;0时，存在
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="费马定理" POSITION="left" ID="ID_677034224" CREATED="1661658705328" MODIFIED="1661658711904">
+<edge COLOR="#dd0000"/>
+</node>
+<node TEXT="罗尔定理" POSITION="left" ID="ID_1579826775" CREATED="1661658712597" MODIFIED="1661658720616">
+<edge COLOR="#7cdddd"/>
+</node>
+<node TEXT="拉格朗日中值定理" POSITION="left" ID="ID_973638233" CREATED="1661658722788" MODIFIED="1661658732409">
+<edge COLOR="#dddd7c"/>
+</node>
+<node TEXT="柯西中值定理" POSITION="left" ID="ID_107442046" CREATED="1661658733588" MODIFIED="1661658741854">
+<edge COLOR="#dd7cdd"/>
+</node>
+<node TEXT="泰勒公式" POSITION="left" ID="ID_405482519" CREATED="1661658742872" MODIFIED="1661658759926">
+<edge COLOR="#7cdd7c"/>
+<node TEXT="带拉格朗日余项的n阶泰勒公式" ID="ID_154904923" CREATED="1661658775056" MODIFIED="1661658806063"/>
+<node TEXT="带佩亚诺余项的n阶泰勒公式" ID="ID_596079068" CREATED="1661658807445" MODIFIED="1661658840522"/>
+</node>
+<node TEXT="积分中值定理" POSITION="left" ID="ID_637661541" CREATED="1661658846412" MODIFIED="1661658856535">
+<edge COLOR="#dd7c7c"/>
+</node>
+</node>
+</map>
